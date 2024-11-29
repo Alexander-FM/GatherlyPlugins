d3019cda372ad0c2871021c02b7194455d97bab1
Alexander Tutoriales <fuentesalexander61@gmail.com>
2024-11-28 22:53:14 -0500

    New changes in module

--- src/main/java/com/codecorecix/gatherly/customer/api/dto/request/CustomerRequestDto.java
+++ src/main/java/com/codecorecix/gatherly/customer/api/dto/request/CustomerRequestDto.java
@@ -1,5 +1,12 @@
 package com.codecorecix.gatherly.customer.api.dto.request;
 
-public class CustomerRequestDto {
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class CustomerRequestDto implements Serializable {
+
+  private Integer id;
 
 }
